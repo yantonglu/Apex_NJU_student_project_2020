4e40ed6c36e013b498a966d67c69bb265863cb89
18whl-njucs <63088309+18whl-njucs@users.noreply.github.com>
2020-04-09 18:09:11 +0800

    add the a+b/a-b testcase

---
+++ testcase/a+b/src_a+b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int a = 1,b = 2;
+  int c = a + b;
+  printf ("%d\n", c);
+  return 0;
+}
+
---
+++ testcase/a+b/src_a-b.c
@@ -0,0 +1,9 @@
+#include"stdio.h"
+
+int main(void)
+{
+  int a = 1, b = 2;
+  int c = a - b;
+  printf("%d\n", c);
+  return 0;
+}
