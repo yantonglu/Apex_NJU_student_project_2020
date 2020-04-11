d6082ba27d236e203dc8763fc308e84c19d2f643
txb-nju <1046562294@qq.com>
2020-04-11 16:54:38 +0800

    Create SWAP_w.c

---
+++ testcase/SWAP/SWAP_w.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+int a[7] = {1,3,10,8,7,9,5};
+void SWAP(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(){
+	SWAP(&a[1], &a[3]);
+	int result = a[1];
+	printf("%d" , result);
+	return 0;
+}
