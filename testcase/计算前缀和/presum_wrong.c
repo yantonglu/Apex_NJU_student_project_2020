1aea3e4d6e066673508d16aa3c8d553df26fae9b
txb-nju <1046562294@qq.com>
2020-04-15 17:34:22 +0800

    Create presum_wrong.c

---
+++ testcase/计算前缀和/presum_wrong.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a[6] = {5, 6, 3, 4, 5};
+int pre_sum[6] = {0, 0, 0, 0, 0, 0};
+int main(void)
+{
+int i = 0;
+for(i = 1; i < 6; i++){
+pre_sum[i] = pre_sum[i - 1] + a[i]; 
+}
+for(i = 1; i < 6; i++)
+printf("%d ", pre_sum[i]);
+return 0;
+}
